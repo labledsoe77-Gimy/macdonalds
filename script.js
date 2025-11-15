8c7e74f037be70dbbfd5bcc0b42f05d56a97112e
labledsoe77-Gimy <labledsoe77@gmail.com>
2025-11-15 12:29:56 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("myButton").addEventListener("click", function() {
+    const message = document.getElementById("message");
+    message.textContent = "You clicked the button! 🎉";
+});
